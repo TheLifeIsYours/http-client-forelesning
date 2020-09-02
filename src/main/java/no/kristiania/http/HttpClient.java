fae1bc2f629f09699167943143af877e42fd0c3c
Johannes Brodwall <johannes.brodwall@soprasteria.com>
2020-09-02 15:10:14 +0200

    Completed reading of response headers

--- src/main/java/no/kristiania/http/HttpClient.java
+++ src/main/java/no/kristiania/http/HttpClient.java
@@ -2,10 +2,13 @@ package no.kristiania.http;
 
 import java.io.IOException;
 import java.net.Socket;
+import java.util.HashMap;
+import java.util.Map;
 
 public class HttpClient {
 
     private int responseCode;
+    private final Map<String, String> responseHeaders = new HashMap<>();
 
     public HttpClient(String hostname, int port, String requestTarget) throws IOException {
         Socket socket = new Socket(hostname, port);
@@ -16,16 +19,30 @@ public class HttpClient {
                 "\r\n";
         socket.getOutputStream().write(request.getBytes());
 
+        String responseLine = readLine(socket);
+        String[] responseLineParts = responseLine.split(" ");
+        responseCode = Integer.parseInt(responseLineParts[1]);
+
+        String headerLine;
+        while (!(headerLine = readLine(socket)).isEmpty()) {
+            int colonPos = headerLine.indexOf(':');
+            String fieldName = headerLine.substring(0, colonPos);
+            String fieldValue = headerLine.substring(colonPos+1).trim();
+            responseHeaders.put(fieldName, fieldValue);
+        }
+    }
+
+    private String readLine(Socket socket) throws IOException {
         StringBuilder line = new StringBuilder();
         int c;
         while ((c = socket.getInputStream().read()) != -1) {
-            if (c == '\n') {
+            if (c == '\r') {
+                socket.getInputStream().read(); // read and discard the following \n
                 break;
             }
             line.append((char)c);
         }
-        String[] responseLineParts = line.toString().split(" ");
-        responseCode = Integer.parseInt(responseLineParts[1]);
+        return line.toString();
     }
 
     public static void main(String[] args) throws IOException {
@@ -40,6 +57,6 @@ public class HttpClient {
     }
 
     public String getResponseHeader(String headerName) {
-        return null;
+        return responseHeaders.get(headerName);
     }
 }
