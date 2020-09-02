487fa2c6a7721517db70a7c6c16fc040c418ebf4
Johannes Brodwall <johannes.brodwall@soprasteria.com>
2020-09-02 13:32:46 +0200

    Complete first version of QueryString

--- src/main/java/no/kristiania/http/QueryString.java
+++ src/main/java/no/kristiania/http/QueryString.java
@@ -1,21 +1,22 @@
 package no.kristiania.http;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class QueryString {
-    private final String parameterValue;
-    private final String parameterName;
+    private final Map<String, String> parameters = new HashMap<>();
 
     public QueryString(String queryString) {
-        int equalPos = queryString.indexOf('=');
-        parameterName = queryString.substring(0, equalPos);
-        parameterValue = queryString.substring(equalPos+1);
+        for (String parameter : queryString.split("&")) {
+            int equalPos = parameter.indexOf('=');
+            String parameterName = parameter.substring(0, equalPos);
+            String parameterValue = parameter.substring(equalPos + 1);
+            parameters.put(parameterName, parameterValue);
+        }
     }
 
-    public String getParameter(String status) {
-        if(status.equals(parameterName)) {
-            return parameterValue;
-        } else  {
-            return null;
-        }
+    public String getParameter(String parameterName) {
+        return parameters.get(parameterName);
     }
 
 
